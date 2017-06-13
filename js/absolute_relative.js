5244b93cd503d7c16498d63ffc4f425e882969c4
elsesir <sirandelsa@163.com>
2017-06-14 02:10:25 +0800

    定位/回流/动画

--- js/absolute_relative.js
+++ js/absolute_relative.js
@@ -1,7 +1,15 @@
 /**
  * Created by bigWhite on 17/6/12.
  */
+
 window.onload=function() {
+    // $("#btnLeftSide").bind("click", function() {
+    //     $("#leftCon").append('<img src="../img/阴阳师-神乐.png" height="200" alt="ShenYue" />');
+    // });
+    // $("#btnRightSide").bind("click", function() {
+    //     $("#rightCon").append('<img src="../img/阴阳师-晴明.png" height="200" alt="QingMing" />');
+    // });
+
     var oTestImg = document.getElementById("testImg"),
         oTestBtn = document.getElementById("testBtn");
 
@@ -15,4 +23,13 @@ window.onload=function() {
             this.innerHTML = "改为absolute";
         }
     };
+
+    // var funShowDisplay = function() {
+    //     $("#displayDisplay").text("现在的display属性值是：" + $("#tableCell").css("display"));
+    // };
+    // funShowDisplay();
+    // $("#button").click(function() {
+    //     $("#tableCell").toggle();
+    //     funShowDisplay();
+    // });
 }
