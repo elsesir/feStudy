7a8d1f914e8ae80690bcc13aa31e7d1fa243ebbc
elsesir <sirandelsa@163.com>
2017-09-17 21:18:20 +0800

    let和const和var的区别

--- js/js_study.js
+++ js/js_study.js
@@ -2,33 +2,13 @@
  * Created by bigWhite on 17/8/8.
  */
 //call /apply/bind函数学习
-/*Function.prototype.bind = function (oThis) {
-         if (typeof this !== "function") {
-               // closest thing possible to the ECMAScript 5 internal IsCallable function
-              throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-             }
-
-         var aArgs = Array.prototype.slice.call(arguments, 1),//获取bine传入参数数组，去除第一个参数
-                 fToBind = this,//在fBound里引用这里的this
-                 fNOP = function () {},
-                 fBound = function () {
-                   return fToBind.apply(this instanceof fNOP && oThis ? this : oThis || window,
-                                           aArgs.concat(Array.prototype.slice.call(arguments))); // 将aArgs添加到fBound参数数组末尾(concat)
-                };
-
-         fNOP.prototype = this.prototype;
-         fBound.prototype = new fNOP();//原型链fBound->this
-
-         return fBound;//新生产的函数
- };*/
-Function.protptype.bind = function(context) {
+Function.protptype.bind2 = function(context) {
     var aArgs = Array.prototype.slice.call(arguments,1);
     var self = this;
     F = function () {};
     bound =  function () {
         var innerArgus = Array.prototype.slice.call(arguments);//可以接受参数
         var finalArgus = aArgs.concat(arguments);//这时的argument是调用bind函数的对象的参数
-        //return self.apply(context, finalArgus);
         return self.apply((this instanceof F ? this : context),finalArgus);
     };
     //为了方便构造函数的使用，实现传递原型链
@@ -55,6 +35,214 @@ Function.prototype.bind = function (oThis) {
     return fBound;
 
 };
+var obj = { a:2, b:3 , c:4};
+//alert(test.sum.call(obj,7,8,9).result);
+//alert(test.sum.apply(obj, [ 9,0,1]));
+var sum2 = test.sum;
+//sum2();//sum2的this指向window
+var sum = test.sum.bind(obj,1,3);//sum的this指向obj
+alert( sum(2) );
+//alert(test.sum(1,2,3).result);
+//alert(binds(test.sum,obj,5,6,7));
+
+/*  参数：待检测字符串；返回：json格式信息）
+ *     思路：URL一般语法格式为：
+ *     protocol :// hostname[:port] / path / [;parameters][?query]#fragment
+ * 使用函数split()分割输入字符串。
+ * 首先以'?'分割为两部分，[0]主要是协议主机信息，[1]为查询信息；
+ * [0]:以'://'分割出协议信息，'/'分割主机和路径。':'分割端口号；
+ * [1]:以'#'分割Query和fragment,'&'分割Query各信息。
+ * */
+function parseUrl(url) {
+    var i;
+    var str0 = url.split('?');      //以'？'区分查询语句和地址信息
+    var str1 = str0[0].split('://');//以'://'区分协议名和主机信息
+    var url_protocol = str1[0];     //获取协议名
+    var str2 = str1[1].split('/');  //以'/'区分主机和路径
+    var url_host ,url_port;
+    if( str2[0].indexOf(':'))       //以':'区分主机和端口号
+    {
+        url_host = str2[0].split(':')[0];
+        url_port = str2[0].split(':')[1];
+    } else {
+        url_host = str2[0];
+    }
+    var url_path = '/';             //获取路径名
+    for( i=1 ; i< str2.length;i++) {
+        url_path =url_path + str2[i] + '/';
+    }
+    var url_query = {};            //获取查询信息
+    var str3 = str0[1].split("#"); //以'#'区分查询信息和信息片段
+    var url_fragment = str3[1];
+    var str4 = str3[0].split('&'); //以'&'区分查询信息各字段
+    for(i=0; i<str4.length; i++)   //查询字段
+    {
+        var temp = str4[i].split('=');
+        url_query[temp[0]] = temp[1];
+    }
+    return {
+        protocol : url_protocol,
+        host: url_host,
+        port: url_port,
+        path: url_path,
+        query: url_query,
+        fragment: url_fragment
+    }
+}
+
+var url = "http://www.taobao.com/php/index.php?key=123456&name=xiangruding&value=20170308&mid=1830246&token=loyyehkvk";
+// console.log(parseUrl(url));
+/*  参数：待检测字符串；返回：1（正确），0（错误）
+ *     思路：设立一个存放括号信息的栈，存放左侧括号信息"( 、 < 、{ 、[ ",逐个字符检测，
+ * 遇到左侧括号则入栈；遇到右侧括号则于栈顶字符比较，如栈非空且成对则将栈顶字符出栈，否则返回0；
+ * 当字符串逐个检测完毕后，栈中无信息，则返回1，表示输入字符串正确；否则，返回0.
+ *     问题：题目对大括号的处理条件不清晰，"}{"也会被识别为错误，是指只要大括号为成对出现就不为错？
+ * 还是只有给出情况"}{"不为错？"}({)"是否判错？因此函数按照和其他括号同样的方式处理的，
+ * 即"}{"也返回0。*/
+function verify(str) {
+    var matchstr= new Array;//存储括号信息的栈
+    var i;
+    for(i= 0; i<str.length; i++) {
+        //遇到左侧括号入栈
+        if(str[i] == '(' || str[i] == '{' || str[i] == '[' || str[i] == '<') {
+            matchstr.push(str[i]);
+        } else if(str[i] == ')') { //遇到右侧括号信息且栈不空则比较是否成对
+            if(matchstr.length !=0 && matchstr[matchstr.length-1]=='('){
+                matchstr.pop();//栈不空则比较且成对则栈顶出栈
+            } else {
+                return 0;  //否则返回0
+            }
+        } else if( str[i] == '}') {
+            if(matchstr.length !=0 && matchstr[matchstr.length-1]=='{'){
+                matchstr.pop();
+            } else {
+                return 0;
+            }
+
+        } else if( str[i] == ']') {
+            if(matchstr.length !=0 && matchstr[matchstr.length-1]=='['){
+                matchstr.pop();
+            } else {
+                return 0;
+            }
+        }
+        else if( str[i] == '>') {
+            if(matchstr.length !=0 && matchstr[matchstr.length-1]=='<'){
+                matchstr.pop();
+            } else {
+                return 0;
+            }
+        }
+    }
+    if(matchstr.length == 0) { //如果输入字符串检测完毕而信息栈已空，则输入信息正确
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+// console.log(verify("---(++++)----"));
+
+//拷贝
+//深拷贝
+var obj = { a:1, arr: [2,3] };
+var shallowObj = shallowCopy(obj);
+function deepClone (obj,c) {
+    var newobj = obj || {};
+    if (typeof obj !== 'object') {
+        return;
+    } else if (window.JSON) {
+        str = JSON.stringify(obj); //系列化对象
+        newobj = JSON.parse(str); //还原
+    } else {
+        for (var i in obj) {
+            newobj[i] = typeof obj[i] === 'object' ?
+                cloneObj(obj[i]) : obj[i];
+        }
+    }
+    return newobj;
+}
+//浅拷贝
+function shallowCopy(src) {
+    var dst = {};
+    for (var prop in src) {
+        if (src.hasOwnProperty(prop)) {
+            dst[prop] = src[prop];
+        }
+    }
+    return dst;
+}
+var china = {
+    nation : '中国',
+    birthplaces:{
+        1:'北京',
+        2:'上海',
+        3:'广州'},
+    skincolr :'yellow',
+    friends: {
+        nation:'中国'
+    }
+
+};
+var meimei = {
+    nation : 'china',
+    birthplaces:'浙江',
+    skincolr :'yellow',
+    friends:{
+        name : '李磊',
+        nation: '中国'     }
+};
+var cc = deepClone(china);
+var ca = shallowCopy(china);
+//  console.log(cc);
+//   console.log(ca);
+//合并两个对象
+function mixin(source, obj1){
+    var source = source ||{};//o是否存在
+    var i,p,obj1;
+    for(i=0;i<arguments.length;i++) {//通过两个合并的方式，实现多个(>2)个数组合并
+        obj1 = arguments[i+1];
+        for (p in arguments[i+1]){
+            if(obj1.hasOwnProperty(p) ){//不考虑原型的属性
+                if(!source.hasOwnProperty(p) )
+                {
+                    source[p]= (typeof obj1[p] === "object")? mixin(source[p],obj1[p]):obj1[p];//类似深拷贝
+                }
+                else if(typeof source[p] === "object"){
+                    mixin(source[p],obj1[p]);
+                }
+            }
+        }
+    }
+    return source;
+}
+var a = {
+    a1: 10,
+    a2: 20,
+    a3: {
+        c1: 10,
+        c2: 30
+    },
+    a4: 41
+};
+
+var b = {
+    a3: {
+        c4: 40
+    },
+    a4: 40
+};
+var c = {
+    a5: {
+        c4: 40
+    }
+};
+
+mixin(a , b);
+//console.log(a);
+// console.log(meimei);
+var lilei = mixin(lilei,china);
+//console.log(lilei);
 
 
 
@@ -85,64 +273,48 @@ var c = 30;
 };
 
 
-var obj = { a:2, b:3 , c:4};
-//alert(test.sum.call(obj,7,8,9).result);
-//alert(test.sum.apply(obj, [ 9,0,1]));
-var sum2 = test.sum;
-//sum2();//sum2的this指向window
-var sum = test.sum.bind(obj,1,3);//sum的this指向obj
-alert( sum(2) );
-//alert(test.sum(1,2,3).result);
-//alert(binds(test.sum,obj,5,6,7));
+
 
 // //bom学习
 // //重复达到最大值后取消间歇调用。
 // /*var num = 0;
-// var max = 5;
-// var intervalId = null;
-// function incrementNumber(){
-//      num++;
-//      // if(num == max) {
-//      //     clearInterval(intervalId);
-//      //     alert("done");
-//      // }
-//     if (num < max)  {
-//         setTimeout(incrementNumber,500);
-//        // alert(num);
-//     } else {
-//         alert("Done");
-//     }
-//  }
-//  setTimeout(incrementNumber,500);
-//  //intervalId = setInterval ( incrementNumber, 500) ;
-// */
+var max = 5;
+var intervalId = null;
+function incrementNumber(){
+     num++;
+     // if(num == max) {
+     //     clearInterval(intervalId);
+     //     alert("done");
+     // }
+    if (num < max)  {
+        setTimeout(incrementNumber,500);
+       // alert(num);
+    } else {
+        alert("Done");
+    }
+ }
+ setTimeout(incrementNumber,500);
+ //intervalId = setInterval ( incrementNumber, 500) ;
+
 // //location对象
-// /*function getQueryStringArgs() {
-//     var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
-//     args = {},
-//     items = qs.length ? qs.split("&") : [],
-//         item = null,
-//         name = null,
-//         value = null,
-//         i =0,
-//         len = items.length;
-//     for (i=0; i<len; i++) {
-//         item = item[i].split("=");
-//         name = decodeURLComponent(item[0]);
-//         value = decodeURLComponent(item[1]);
-//
-//         if (name.length) {
-//             args[name] = value;
-//         }
-//     }
-//     return args;
-// }*/
-//
+/*function getQueryStringArgs() {
+    var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+    args = {},
+    items = qs.length ? qs.split("&") : [],
+        item = null,
+        name = null,
+        value = null,
+        i =0,
+        len = items.length;
+    for (i=0; i<len; i++) {
+        item = item[i].split("=");
+        name = decodeURLComponent(item[0]);
+        value = decodeURLComponent(item[1]);
 
-function people() {
-    console.log(this);
-}
+        if (name.length) {
+            args[name] = value;
+        }
+    }
+    return args;
+}*/
 
-var p = new people;
-
-console.log(p instanceof people);
---
+++ js/let_var_const.js
@@ -0,0 +1,137 @@
+/**
+ * Created by bigWhite on 17/9/17.
+ */
+/*var a=1;
+ console.log(1);
+ setTimeout(function(){console.log(2)},0);
+ console.log(3);
+ */
+/*   var arr = [];  // arr = { 0: xx , 1: xx }
+ function outer() {
+ var i = 0;
+ for (; i < 10; i++) {
+ arr[i] = function inner() {
+ console.log(this);
+ this.b = 'xby';
+ return i;
+ }
+ }
+ }
+ outer();
+ arr[9]();//this->arr;
+ // console.log(arr);
+ var i = 10;
+ function inner () {
+ var i = 100;
+ var a = {
+ i: 2,
+ b: function () {
+ console.log(i);
+ },
+ c: {
+ i: 1
+ }
+ };
+ a.b();
+ }
+ inner();
+
+
+ */
+/*
+//var 和 let 区别,1.let块级作用域，var 函数作用域
+ var a = [];
+ for(var i=0;i<10;i++) {
+ a[i] = function() {
+ console.log(i);
+ };
+ }
+ a[6]();//10
+ for (let i=0;i<10;i++) {
+ a[i] = function() {
+ console.log(i);
+ };
+ }
+ a[5]();//5
+
+//let 解决闭包问题：var 输出为10；let输出为1-10；
+ function createfunction() {
+ var a = new Array();
+ for (let i = 0; i < 10; i++) {
+    a[i] = function () {
+        return i;
+        };
+    }
+    return a;
+ }
+ createfunction();
+ var b = createfunction();
+ for(var i=0;i<10;i++) {
+     console.log(b[i]());
+ }
+ //2.let不会变量提升,const 也不会变量提升
+(function () {
+    console.log(i);
+    var i = 0;
+    console.log(j);
+    let j = 0;
+    console.log(h);
+    const h = 3;
+})();
+//3.暂时性死区
+var tmp=123;
+if(true) {
+    tmp = "abc";
+    let tmp ;
+}//报错
+ var tmp=123;
+ if(true) {
+ tmp = "abc";
+ var  tmp ;
+ console.log(tmp);//abc
+ }
+ console.log(tmp);//abc
+
+//4.不允许重复声明
+function func1() {
+    let i = 0;
+    var i = 2;
+    console.log(i);
+}
+func1();//运行报错
+
+function add (num) {
+    let num = 12;
+    console.log(num);
+    return num;
+}
+add(1);
+function ant () {
+    let nu = 1;
+    let nu =2;
+    console.log(nu);
+}
+ant();*/
+
+//const常量：块级作用域，声明时必须赋值，赋值不可改变，不可重复声明，变量不提升，
+//const只保证变量名指向的地址不变，不保证该地址的数据不变。
+/*const a = { age: 13};
+a.name = "xby";
+console.log(a);
+const b = [];
+b.push("love");
+console.log(b);
+const c = {};
+c = { name: "re"};
+//const如果希望将本身冻结，可以使用Object.freeze()方法
+const  d = Object.freeze({});
+d.name = "like";
+console.log(d);//freeze后不可以改变对象属性，d保持为{}，不报错。*/
+var tmp=123;
+if(true) {
+    tmp = "abc";
+    var  tmp ;
+    console.log(tmp);
+}
+console.log(tmp);
+
